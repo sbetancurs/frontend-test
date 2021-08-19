c1e07027c9a595e1b85a8dd93c3b5cfbdb1ee079
Sebastian Betancur <sebastian.betancur@primona.se>
2021-08-19 12:15:07 -0500

    initial confir with navbar styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroeDetailsComponent } from './components/heroe-details/heroe-details.component';
+import { HeroeCardComponent } from './components/heroe-card/heroe-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeroeDetailsComponent, HeroeCardComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
